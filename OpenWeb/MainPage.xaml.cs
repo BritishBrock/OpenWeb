80ea56e5e7bd2d4dcb9cf22f13cdd0cb867516b1
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-06-26 21:35:45 +0200

    Start of project.

---
+++ OpenWeb/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenWeb;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ OpenWeb/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OpenWeb;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
